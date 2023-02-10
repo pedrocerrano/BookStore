2bacb2b2458a681a273efe36a70d732e40a0cac3
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-09 19:17:45 -0600

    Adds Model

---
+++ BookStore/BookStore/Model/Book.swift
@@ -0,0 +1,25 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by iMac Pro on 2/9/23.
+//
+
+import Foundation
+
+class Book {
+    var author: String
+    var title: String
+    var lastUpdated: Date
+    var rating: Double
+    var synopsis: String
+    
+    init(author: String, title: String, lastUpdated: Date = Date(), rating: Double, synopsis: String) {
+        self.author = author
+        self.title = title
+        self.lastUpdated = lastUpdated
+        self.rating = rating
+        self.synopsis = synopsis
+    } //: MEMBER-WISE INITIALIZER
+    
+} //: CLASS
